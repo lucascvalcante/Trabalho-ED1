b1b56963574cc07119aceda67f0f220f82f6afc7
Lucas <lucaskcavalcante@gmail.com>
2025-10-11 20:26:54 -0300

    feat: Adição do tad carregador

---
+++ src/carregador.c
@@ -0,0 +1,52 @@
+#include "carregador.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+typedef struct Carregador{
+    Pilha formas;
+    LadoCarregador lado;
+}Stcarregador; 
+
+
+Carregador CriarCarregador(LadoCarregador lado){
+    Stcarregador *c = malloc(sizeof(Stcarregador));
+    if(c == NULL){
+        printf("Não foi possivel alocar memória para o carregador!\n");
+        exit(1);
+    } 
+
+    c->formas = Criar_Pilha();
+    if(c->formas == NULL){
+        printf("Não foi possível criar a pilha para o carregador!\n");
+        free(c);
+        exit(1);
+    } 
+    c->lado = lado;
+    
+    return ((Stcarregador*)c);
+} 
+
+void InserirCarregador(Carregador carregador, Forma f){
+    Stcarregador *c = ((Stcarregador*)carregador);
+    
+    InserirPilha(c->formas, f);
+} 
+
+Forma RetirarCarregador(Carregador carregador){
+    Stcarregador *c = ((Stcarregador*)carregador);
+
+    return RetirarPilha(c->formas);
+} 
+
+int GetSizeCarregador(Carregador carregador){
+    Stcarregador *c = ((Stcarregador*)carregador);
+    return SizePilha(c->formas);
+}
+
+void KillCarregador(Carregador carregador){
+    Stcarregador *c = ((Stcarregador*)carregador);
+
+    KillPilha(c->formas);
+    free(c);
+}
---
+++ src/carregador.h
@@ -0,0 +1,46 @@
+#ifndef CARREGADOR_H
+#define CARREGADOR_H
+#include "pilha.h"
+#include "formas.h"
+
+typedef enum{
+    LadoEsquerdo,
+    LadoDireito
+}LadoCarregador;
+
+typedef void *Carregador;
+/*
+    Arquivo .h relacionado a um carregador, local onde as formas serão retiradas para serem disparadas
+*/
+
+
+/// @brief Cria e retorna um carregador
+/// @param lado O lado em que o carregador irá operar(direito ou esquerdo)
+/// @return Ponteiro apontando para o carregador
+Carregador CriarCarregador(LadoCarregador lado);
+
+
+/// @brief Adiciona uma forma no carregador
+/// @param c Ponteiro apontando para o carregador
+/// @param f Ponteiro apontando para a forma que será inserida
+void InserirCarregador(Carregador c, Forma f);
+
+
+/// @brief Retira e retorna uma forma que está no topo do carregador
+/// @param c Ponteiro para o carregador
+/// @return A forma retirada
+Forma RetirarCarregador(Carregador c);
+
+
+/// @brief Retorna o número de formas que está presente no atual momento no carregador
+/// @param c Ponteiro para o carregador
+/// @return Número de formas
+int GetSizeCarregador(Carregador c);
+
+
+/// @brief Libera toda a memória alocada pelo carregador
+/// @param c Ponteiro para o carregador
+void KillCarregador(Carregador c);
+
+
+#endif
